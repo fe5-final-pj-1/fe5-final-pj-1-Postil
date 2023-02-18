6d222591226cb3d1fdce88eff14debe754c1c47e
Ramin Kuraishy <ramin.kuraishy@gmail.com>
2023-02-18 21:20:55 +0200

    test

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     // eslint-disable-next-line react/no-unescaped-entities
-    return <header>I'm header and I wish good luck everybody!</header>;
+    return <header>I'm header and I wish good luck dfvdfvdfvdf!</header>;
 }
 
 export default Header;
