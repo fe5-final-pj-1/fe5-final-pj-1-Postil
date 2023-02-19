a19cbb5e8f2ec18959d82fb6c7ed78731e485cc6
Serhii Siedov <sergey.sedov88@gmail.com>
2023-02-19 14:58:30 +0200

    create button component and basic mixins/variables

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Button() {
-    return <button></button>;
+function Button({ handleClick, text, background, color, border, borderRadius, padding }) {
+    return (
+        <button style={{ background, color, border, borderRadius, padding }} onClick={handleClick}>
+            {text}
+        </button>
+    );
 }
 
 export default Button;
+
+Button.propTypes = {
+    handleClick: PropTypes.func,
+    text: PropTypes.string,
+    background: PropTypes.string,
+    color: PropTypes.string,
+    border: PropTypes.string,
+    borderRadius: PropTypes.oneOf(PropTypes.string, PropTypes.number),
+    padding: PropTypes.oneOf(PropTypes.string, PropTypes.number),
+};
+
+Button.defaultProps = {
+    handleClick: () => {},
+    text: '',
+    background: 'transparent',
+    color: '#000000',
+    border: 'none',
+    borderRadius: 'none',
+    padding: 0,
+};
