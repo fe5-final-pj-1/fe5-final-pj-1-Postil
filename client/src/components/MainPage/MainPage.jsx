68c1c6ab99eb75b8bc6f9a1198ff19770fad250f
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2023-02-21 14:01:44 +0200

    added component Popular

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CarouselSection from '../CarouselSection/CarouselSection';
+import PopularSection from '../PopularSection';
 
 function MainPage() {
     return (
         <main>
             <CarouselSection />
+            <PopularSection />
         </main>
     );
 }
