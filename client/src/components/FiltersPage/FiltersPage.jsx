61964dd7fd4b6c02afa21f4dfacced584d6629ff
Serhii Siedov <sergey.sedov88@gmail.com>
2023-02-21 23:32:03 +0200

    create tabs & filters

--- client/src/components/FiltersPage/FiltersPage.jsx
+++ client/src/components/FiltersPage/FiltersPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './FiltersPage.module.scss';
+import Tabs from './Tabs';
+import Filters from './Filters';
 
 function FiltersPage() {
-    return <main></main>;
+    return (
+        <main className={classnames(styles.wrapper, 'container')}>
+            <article className={styles.left}>
+                <h1>CATALOG</h1>
+                <Filters />
+            </article>
+            <section className={styles.right}>
+                <Tabs />
+            </section>
+        </main>
+    );
 }
 
 export default FiltersPage;
--- client/src/components/Icon/Icon.jsx
+++ client/src/components/Icon/Icon.jsx
@@ -66,6 +66,35 @@ function Icon(props) {
                     />
                 </svg>
             );
+        case 'plus':
+            return (
+                <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M8 0H6V6H0V8H6V14H8V8H14V6H8V0Z"
+                        fill="#373F41"
+                    />
+                </svg>
+            );
+        case 'minus':
+            return (
+                <svg
+                    width="14"
+                    height="2"
+                    viewBox="0 0 14 2"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M0 0H14V2H0V0Z" fill="#373F41" />
+                </svg>
+            );
         default:
             return null;
     }
