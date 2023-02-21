3b0072669909ffad64bde92bbf37b4ddafbd2f9b
Ramin Kuraishy <ramin.kuraishy@gmail.com>
2023-02-21 20:44:43 +0200

    add snapshot

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import Header from './Header';
 
-describe('Header component', () => {
-    let wrapper;
-    
-    beforeEach(() => {
-      wrapper = shallow(<Header />);
-    });
-  
-    it('should toggle the header menu on clicking the menu icon', () => {
-      expect(wrapper.state().showMenu).toEqual(false);
-      wrapper.find('.openBtn').simulate('click');
-      expect(wrapper.state().showMenu).toEqual(true);
-    });
-  
-    it('should update search box value on input', () => {
-      const input = wrapper.find('.input');
-      input.simulate('change', { target: { value: 'Test Search' }});
-      expect(input.prop('value')).toEqual('Test Search');
-    });
-  
-  });
+test('Icon snapshot', () => {
+    const view = render(<Header/>);
+    expect(view).toMatchSnapshot();
+});
