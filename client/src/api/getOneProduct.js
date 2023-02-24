9c3fb43025f9a82abb92ba81945a105124233c0a
Dmytro Didenko <didenko.dma@gmail.com>
2023-02-24 08:54:05 +0300

    Fix problems with file naming, change file structure, add .env for client folder

--- client/src/api/getOneProduct.js
+++ client/src/api/getOneProduct.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 async function getOneProduct(productId) {
+    const REACT_APP_URL_API = process.env.REACT_APP_URL_API;
     try {
-        const response = await axios.get(`http://127.0.0.1:5000/api/products/${productId}`);
+        const response = await axios.get(`${REACT_APP_URL_API}/products/${productId}`);
         return response;
     } catch (error) {
         console.error(error.message);
