ab3865857059acdd0d27e1e606feb58c8a935fe3
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2023-02-24 17:48:08 +0200

    added files to component checkout

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CarouselMainSection from '../CarouselMainSection/CarouselMainSection';
 import PopularSection from '../PopularSection';
 import NewInSection from '../NewInSection';
-import PaymentSection from '../CheckoutPage/PaymentSection/PaymentSection';
 
 function MainPage() {
     return (
@@ -10,7 +9,6 @@ function MainPage() {
             <CarouselMainSection />
             <NewInSection />
             <PopularSection />
-            <PaymentSection />
         </main>
     );
 }
