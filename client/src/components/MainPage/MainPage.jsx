3374be7d60bff6e3892652da33e69765f69afc00
Юрий Павлов <pavlovyuriypho@gmail.com>
2023-02-26 15:47:17 +0200

    some-fix

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CarouselMainSection from '../CarouselMainSection/CarouselMainSection';
 import PopularSection from '../PopularSection';
 import NewInSection from '../NewInSection';
-import SingleItemSection from '../SingleItemSection/SingleItemSection';
 
 function MainPage() {
     return (
