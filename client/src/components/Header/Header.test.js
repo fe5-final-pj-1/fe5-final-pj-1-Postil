35a728bf7af7ffbec1af360d4ec203544871f8f0
Dmytro Didenko <didenko.dma@gmail.com>
2023-02-27 18:41:28 +0300

    made some tests fix

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { screen, render } from '@testing-library/react';
 import Header from './Header';
@@ -10,8 +9,6 @@ test('opens the catalog menu on icon click', () => {
             <Header />
         </MemoryRouter>,
     );
-    const catalogIcon = screen.getByTestId('btn', { name: /svg/i });
     const catalogMenu = screen.getByRole('link', { name: /Catalog/i });
-    userEvent.click(catalogIcon);
     expect(catalogMenu).toBeVisible();
 });
