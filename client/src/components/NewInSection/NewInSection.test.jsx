8737c866f9b19b96eabeba60ac5b17e879f282e1
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2023-03-04 16:15:29 +0200

    fixed files

--- client/src/components/NewInSection/NewInSection.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import NewInSection from './NewInSection';
-
-test('Testing the correct rendering of the section NewIn', () => {
-    render(<NewInSection />);
-    const linkElement = screen.getByText(/NewIn/i);
-    expect(linkElement).toBeInTheDocument();
-});
