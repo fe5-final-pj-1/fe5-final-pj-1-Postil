ed780635ceaece6ed846a850b81f0f4c4634d48a
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2023-03-10 10:19:43 +0200

    fixed request

--- client/src/components/Checkout/sections/SummaryTextSection/SummaryTextSection.jsx
+++ client/src/components/Checkout/sections/SummaryTextSection/SummaryTextSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './SummaryTextSection.module.scss';
 import { Link } from 'react-router-dom';
+
 const SummaryTextSection = ({ subtotal, route }) => {
     return (
         <>
