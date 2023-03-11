ea050ef82e8546d58c1933488aae28c0ab43e72a
Dmytro Didenko <didenko.dma@gmail.com>
2023-03-11 12:15:37 +0300

    add many fixes

---
+++ client/src/routes/AdminDashboardCustomersRoute.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AdminDashboardCustomers from 'components/AdminPanel/AdminDashboardCustomers';
+
+function AdminDashboardCustomersRoute() {
+    return <AdminDashboardCustomers />;
+}
+
+export default AdminDashboardCustomersRoute;
