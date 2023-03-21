e895cb069721ead5f7089b9f84e84cfa7d84dbfd
Develop2Den <115218139+Develop2Den@users.noreply.github.com>
2023-03-21 18:11:28 +0200

    tests files

---
+++ client/src/components/Checkout/pages/BagPage/BagPage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import BagPage from './BagPage';
+import { Provider } from 'react-redux';
+import store from '../../../../store/store';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('BagPage', () => {
+    test('renders the BagPage component', () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <BagPage />
+                </BrowserRouter>
+            </Provider>,
+        );
+        const bagPageElement = screen.getByTestId('bag-page');
+        expect(bagPageElement).toBeInTheDocument();
+    });
+});
---
+++ client/src/components/Checkout/pages/ShippingDetailsPage/ShippingDetailsPage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../../../../store/store';
+import { BrowserRouter } from 'react-router-dom';
+import ShippingDetailsPage from './ShippingDetailsPage';
+
+describe('ShippingDetailsPage', () => {
+    test('renders the ShippingDetailsPage component', () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ShippingDetailsPage />
+                </BrowserRouter>
+            </Provider>,
+        );
+        const bagPageElement = screen.getByTestId('shipping-page');
+        expect(bagPageElement).toBeInTheDocument();
+    });
+});
