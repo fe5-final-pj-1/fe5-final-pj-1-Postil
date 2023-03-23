cd3371742aeb43396f8769e966cd1d767cee4cac
Serhii Siedov <sergey.sedov88@gmail.com>
2023-03-23 15:10:06 +0200

    customer orders finally

--- client/src/components/UserOrdersPage/UserOrdersPage.jsx
+++ client/src/components/UserOrdersPage/UserOrdersPage.jsx
@@ -6,9 +6,7 @@ import Icon from '../Icon/Icon';
 import getCustomerOrder from 'api/getCustomerOrder';
 
 function UserOrdersPage() {
-    const [active, setActive] = useState({
-        orderData: false,
-    });
+    const [active, setActive] = useState({});
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
@@ -28,7 +26,8 @@ function UserOrdersPage() {
         <main>
             <div className="container">
                 <p className={styles.title}>My orders</p>
-                {orders.map((item, index) => {
+                {[...orders].reverse().map((item, index) => {
+                    console.log(item);
                     return (
                         <div className={styles.boxInfo} key={index}>
                             <Button
@@ -51,28 +50,88 @@ function UserOrdersPage() {
                                 }
                             />
                             {active[item.customOrderNumder] && (
-                                <div className={styles.wrpFullItem}>
-                                    <ul className={styles.orderDataList}>
-                                        <li className={styles.orderDataItem}>
-                                            <span className={styles.orderDataPlaseholder}>
-                                                date of purchase:
-                                            </span>
-                                            {item.date.slice(0, 10)}
-                                        </li>
-                                        <li className={styles.orderDataItem}>
-                                            <span className={styles.orderDataPlaseholder}>
-                                                Status:
-                                            </span>
-                                            {item.status}
-                                        </li>
-                                        <li className={styles.orderDataItem}>
-                                            <span className={styles.orderDataPlaseholder}>
-                                                Total Sum
-                                            </span>
-                                            $ {(item.totalSum * 1.1 + item.shipping).toFixed(2)}
-                                        </li>
-                                    </ul>
-                                </div>
+                                <>
+                                    <div className={styles.wrpFullItem}>
+                                        <ul className={styles.orderDataList}>
+                                            <li className={styles.orderDataItem}>
+                                                <span className={styles.orderDataPlaseholder}>
+                                                    purchase date
+                                                </span>
+                                                {item.date.slice(0, 10)}
+                                            </li>
+                                            <li className={styles.orderDataItem}>
+                                                <span className={styles.orderDataPlaseholder}>
+                                                    Status
+                                                </span>
+                                                {item.status}
+                                            </li>
+                                            <li className={styles.orderDataItem}>
+                                                <span className={styles.orderDataPlaseholder}>
+                                                    Total Sum
+                                                </span>
+                                                $ {(item.totalSum * 1.1 + item.shipping).toFixed(2)}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <table className={styles.productsTable}>
+                                        <thead>
+                                            <tr>
+                                                <th className={styles.productsTableHide}>
+                                                    Product ID
+                                                </th>
+                                                <th>Product Name</th>
+                                                <th className={styles.productsTableHide}>
+                                                    Category
+                                                </th>
+                                                <th>Quantity</th>
+                                                <th>Price</th>
+                                                <th>Fabric</th>
+                                                <th>Color</th>
+                                                <th>Size</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {item.products.map((product) => {
+                                                const cartProduct = product;
+                                                product = product.product;
+                                                return (
+                                                    <tr key={product._id}>
+                                                        <td className={styles.productsTableHide}>
+                                                            {product.itemNo}
+                                                        </td>
+                                                        <td>
+                                                            <img
+                                                                className={styles.productsImg}
+                                                                src={product.imageUrls[0]}
+                                                                alt={`Product ${product._id}`}
+                                                            />
+                                                            <span>{product.name}</span>
+                                                        </td>
+                                                        <td className={styles.productsTableHide}>
+                                                            {product.categories}
+                                                        </td>
+                                                        <td>{cartProduct.cartQuantity}</td>
+                                                        <td>{product.currentPrice}$</td>
+                                                        <td>{product.fabric}</td>
+                                                        <td>
+                                                            {product.color ? (
+                                                                <span
+                                                                    style={{
+                                                                        backgroundColor:
+                                                                            product.color,
+                                                                    }}
+                                                                ></span>
+                                                            ) : (
+                                                                '-'
+                                                            )}
+                                                        </td>
+                                                        <td>{product.size ? product.size : '-'}</td>
+                                                    </tr>
+                                                );
+                                            })}
+                                        </tbody>
+                                    </table>
+                                </>
                             )}
                         </div>
                     );
