3ee34621f37100576b2f03254b3e78ed1865b1f9
Dmytro Didenko <didenko.dma@gmail.com>
2023-03-25 20:10:27 +0300

    add revies-lazy-load to admin panel

--- client/src/components/AdminPanel/AdminDashboardReviews/AdminDashboardReviews.jsx
+++ client/src/components/AdminPanel/AdminDashboardReviews/AdminDashboardReviews.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line no-unused-vars
 import classNames from 'classnames';
+// eslint-disable-next-line no-unused-vars
 import Button from 'components/Button';
+// eslint-disable-next-line no-unused-vars
 import Icon from 'components/Icon';
 import adminPanelStyles from './AdminDashboardReviews.module.scss';
+// eslint-disable-next-line no-unused-vars
 import { Link } from 'react-router-dom';
-import getAllComments from 'api/getAllComments';
+import getLimitedComments from 'api/getLimitedComments';
+import { useInView } from 'react-intersection-observer';
+// eslint-disable-next-line no-unused-vars
 import deleteComment from 'api/deleteComment';
-import { Oval } from 'react-loader-spinner';
+// eslint-disable-next-line no-unused-vars
+import { Oval, ThreeDots } from 'react-loader-spinner';
 
 function AdminDashboardReviews() {
     const [reviews, setReviews] = useState([]);
+    const [startPage, setStartPage] = useState(1);
+    const { ref, inView } = useInView({
+        threshold: 1,
+    });
+    // eslint-disable-next-line no-unused-vars
     const [isLoaded, setIsLoaded] = useState(false);
+    // eslint-disable-next-line no-unused-vars
+    const [isReviewsUpload, setIsReviewsUpload] = useState(true);
+    // eslint-disable-next-line no-unused-vars
     const months = [
         'Jan',
         'Feb',
@@ -26,34 +40,49 @@ function AdminDashboardReviews() {
         'Nov',
         'Dec',
     ];
+
     useEffect(() => {
-        getAllComments().then((res) => {
-            if (res.data) {
-                setReviews(res.data);
-            }
-            setIsLoaded(true);
-        });
-    }, []);
-    if (!isLoaded) {
-        return (
-            <Oval
-                height={130}
-                width={130}
-                color="#373F41"
-                wrapperStyle={{}}
-                wrapperClass={adminPanelStyles.loader}
-                visible={true}
-                ariaLabel="oval-loading"
-                secondaryColor="#4fa94d"
-                strokeWidth={2}
-                strokeWidthSecondary={2}
-            />
-        );
-    }
+        if (inView) {
+            loadMoreComments();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inView]);
+
+    const loadMoreComments = () => {
+        setIsReviewsUpload(false);
+        setTimeout(() => {
+            getLimitedComments(startPage, 10).then((res) => {
+                setReviews((prevReviews) => {
+                    setIsLoaded(true);
+                    setIsReviewsUpload(true);
+                    return [...prevReviews, ...res.data];
+                });
+                setStartPage((prevStartPage) => prevStartPage + 1);
+            });
+        }, 300);
+    };
+
     return (
         <div className={adminPanelStyles.reviews}>
             <div className={adminPanelStyles.reviewsWrapper}>
-                {[...reviews].reverse().map((review) => {
+                {reviews.length === 0 && <div ref={ref}></div>}
+                {!isLoaded && (
+                    <div className={adminPanelStyles.loaderContainer}>
+                        <Oval
+                            height={130}
+                            width={130}
+                            color="#373F41"
+                            wrapperStyle={{}}
+                            wrapperClass={adminPanelStyles.loader}
+                            visible={true}
+                            ariaLabel="oval-loading"
+                            secondaryColor="#4fa94d"
+                            strokeWidth={2}
+                            strokeWidthSecondary={2}
+                        />
+                    </div>
+                )}
+                {[...reviews].map((review, index) => {
                     const date = new Date(review.date);
                     const dateNow = Date.now();
                     const time = date.getTime();
@@ -62,7 +91,8 @@ function AdminDashboardReviews() {
                     const month = date.getMonth();
                     const year = date.getFullYear();
                     return (
-                        <div className={adminPanelStyles.reviewsContainer} key={review._id}>
+                        <div className={adminPanelStyles.reviewsContainer} key={index}>
+                            {index === reviews.length - 1 && <div ref={ref}></div>}
                             <div className={adminPanelStyles.reviewsProductImage}>
                                 <Link to={`/catalog/${review.product._id}`}>
                                     <img
@@ -108,7 +138,7 @@ function AdminDashboardReviews() {
                                     handleClick={() => {
                                         setIsLoaded(false);
                                         deleteComment(review._id).then(() => {
-                                            getAllComments().then((res) => {
+                                            getLimitedComments(1, 10).then((res) => {
                                                 if (res.data) {
                                                     setReviews(res.data);
                                                     setIsLoaded(true);
@@ -125,6 +155,20 @@ function AdminDashboardReviews() {
                     );
                 })}
             </div>
+            {isLoaded && !isReviewsUpload && (
+                <div>
+                    <ThreeDots
+                        height="50"
+                        width="50"
+                        radius="9"
+                        color="#8a9394"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{ justifyContent: 'center' }}
+                        wrapperClassName=""
+                        visible={true}
+                    />
+                </div>
+            )}
         </div>
     );
 }
