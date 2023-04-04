052f776a2d38cd213cd16adaf7df1f6c05c94363
Юрий Павлов <pavlovyuriypho@gmail.com>
2023-04-04 09:30:27 +0300

    fix-CustomerServicePage

--- client/src/pages/CustomerServicePage/CustomerServicePage.jsx
+++ client/src/pages/CustomerServicePage/CustomerServicePage.jsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './CustomerServicePage.module.scss';
+import ServiceTopicItem from './components/ServiceTopicItem/ServiceTopicItem';
+import contentList from './content';
+import classNames from 'classnames';
 
 function CustomerServicePage() {
-    return <div>Hi, I am a Customer Service Page page!</div>;
+    const [isNavVisible, setIsNavVisible] = useState(false);
+    const scrollToAnchor = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const id = e.target.href.split('#')[1];
+        const dest = document.getElementById(id).offsetTop;
+
+        scrollTo({
+            top: dest,
+            behavior: 'smooth',
+        });
+        setIsNavVisible(false);
+    };
+
+    const showHideNavMenu = (e) => {
+        e.stopPropagation();
+        setIsNavVisible((prevState) => !prevState);
+    };
+    const onNavListBlurHandler = (e) => {
+        e.stopPropagation();
+        if (!e.relatedTarget) {
+            setIsNavVisible(false);
+            return;
+        }
+        if (e.relatedTarget.classList.contains(styles.navMenuLink)) return;
+        console.log(e.relatedTarget);
+
+        setIsNavVisible(false);
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.navWrapper}>
+                <h3 className={styles.navHeader}>TERMS OF SERVICE</h3>
+                <nav className={styles.navMenu} role="presentation" onBlur={onNavListBlurHandler}>
+                    <h3>
+                        <button
+                            className={classNames({
+                                [styles.navHeaderBtn]: true,
+                                [styles.navVisible]: isNavVisible,
+                            })}
+                            onClick={showHideNavMenu}
+                        >
+                            TERMS OF SERVICE
+                        </button>
+                    </h3>
+                    <ul
+                        className={classNames({
+                            [styles.navMenuList]: true,
+                            [styles.navVisible]: isNavVisible,
+                        })}
+                    >
+                        {contentList.map(({ id, header }) => (
+                            <li key={id} className={styles.navMenuListItem}>
+                                <a
+                                    href={`#${id}`}
+                                    className={styles.navMenuLink}
+                                    onClick={scrollToAnchor}
+                                >
+                                    {header}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+
+            <ul className={styles.serviceTopicsList}>
+                {contentList.map((item) => (
+                    <li key={item.id} className={styles.topicItem}>
+                        <ServiceTopicItem {...item} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default CustomerServicePage;
